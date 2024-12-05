c4a2376ebcdd5b7ac01d573b8c44a2dd8196df6d
montserrat000 <alemsitoc@gmail.com>
2024-12-04 19:45:39 -0600

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>CiTIM Grupo XB</title>
-    <link rel="stylesheet" href="css/esrilos.css"/>
+    <link rel="stylesheet" href="css/estilos.css"/>
         <link rel="stylesheet" href="css/menu.css"/>
         <link rel="stylesheet" href="css/problemas.css"/>
     <link href="https://fonts.googleapis.com/css?family=Comfortaa" rel="stylesheet">
